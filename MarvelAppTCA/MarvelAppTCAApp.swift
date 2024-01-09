512c47316a52867368913526a92f602dc4e2f6cb
Luis Gustavo <luis.gustavo3013@hotmail.com>
2024-01-09 09:43:47 -0300

    Added TCA library

--- MarvelAppTCA/MarvelAppTCAApp.swift
+++ MarvelAppTCA/MarvelAppTCAApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct MarvelAppTCAApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
