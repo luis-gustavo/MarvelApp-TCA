ce49c51ea011ed3782aaa2eb7532cb8f396f1df3
Luis Gustavo <luis.gustavo3013@hotmail.com>
2024-01-09 09:40:16 -0300

    Created project structure

---
+++ MarvelAppTCA/MarvelAppTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppTCAApp.swift
+//  MarvelAppTCA
+//
+//  Created by Luis Gustavo on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
